7dd600c340a7dc2ca5c61735985049bc0807fc87
lok1e <lok1e@bk.ru>
2024-04-01 15:44:56 +0300

    Правки параграфов при переносе

--- src/widgets/ProfessionModal/ProfessionModal.tsx
+++ src/widgets/ProfessionModal/ProfessionModal.tsx
@@ -9,6 +9,7 @@ import { Zap } from 'lucide-react';
 import { FC, useEffect, useState } from 'react';
 import { fetchCategories } from '@/entities/Professions/api/api.ts';
 import { CategoryInterface } from '@/entities/Professions/api/types.ts';
+import { Glow, GlowCapture } from '@codaworks/react-glow';
 
 const ProfessionModal: FC<ProfessionDescriptionInterface> = ({ id, close }) => {
     const [categories, setCategories] = useState<CategoryInterface[]>([]);
@@ -46,7 +47,7 @@ const ProfessionModal: FC<ProfessionDescriptionInterface> = ({ id, close }) => {
 
                     <div
                         className="flex gap-[140px] justify-between text-[18px]
-                    max-[1130px]:flex-col max-[1130px]:gap-[20px]"
+                    max-[1130px]:flex-col max-[1130px]:gap-[20px] whitespace-pre-wrap"
                     >
                         <div className="flex flex-col gap-[20px] text-white leading-[165%]">
                             <p>{filteredData.description}</p>
@@ -67,18 +68,22 @@ const ProfessionModal: FC<ProfessionDescriptionInterface> = ({ id, close }) => {
                                 content={filteredData.salaryText}
                             />
                         </div>
-                        <div className="flex flex-col gap-[23px] ">
-                            <StatSection title="Восстребованность" />
-                            <StatSection title="Порог входа" />
-                            <div className="flex flex-col text-white gap-[5px]">
-                                <p className="text-[23px]">Зарплата</p>
-                                <div className="flex gap-[9px] items-center justify-center w-[260px] h-[90px] bg-gradient-to-br from-[#EAB308]/50 to-[#271E00]/40 rounded-[18px]">
-                                    <p className="text-[30px]">
-                                        {filteredData.salary}
-                                    </p>
+                        <GlowCapture>
+                            <Glow>
+                                <div className="flex flex-col gap-[23px] ">
+                                    <StatSection title="Восстребованность" />
+                                    <StatSection title="Порог входа" />
+                                    <div className="flex flex-col text-white gap-[5px]">
+                                        <p className="text-[23px]">Зарплата</p>
+                                        <div className="flex gap-[9px] items-center justify-center w-[260px] h-[90px] border border-white rounded-[18px]">
+                                            <p className="text-[30px]">
+                                                {filteredData.salary}
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
+                            </Glow>
+                        </GlowCapture>
                     </div>
                 </DialogContent>
             )}
@@ -91,7 +96,7 @@ const InfoSection: FC<{
     content: string | undefined;
 }> = ({ title, content }) => (
     <div className="flex flex-col gap-[5px]">
-        <p className="font-bold text-[20px]">{title}</p>
+        <p className="font-bold text-[20px] whitespace-pre-wrap">{title}</p>
         <p className="whitespace-pre-wrap">{content}</p>
     </div>
 );
@@ -99,7 +104,7 @@ const InfoSection: FC<{
 const StatSection: FC<{ title: string }> = ({ title }) => (
     <div className="flex flex-col text-white gap-[5px]">
         <p className="text-[23px]">{title}</p>
-        <div className="flex gap-[9px] items-center justify-center w-[260px] h-[90px] bg-gradient-to-br from-[#EAB308]/50 to-[#271E00]/40 rounded-[18px]">
+        <div className="flex gap-[9px] items-center justify-center w-[260px] h-[90px] border border-white rounded-[18px]">
             <Zap size={50} />
             <Zap size={50} />
             <Zap size={50} />
